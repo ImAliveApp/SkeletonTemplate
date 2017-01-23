da1330aefef730f54d33df14e1a42c68e7cf36f4
hay12396 <hay12396@gmail.com>
2017-01-23 11:26:25 +0200

    First commit

---
+++ CharacterTemplate/app.js
@@ -0,0 +1,39 @@
+/// <reference path="Scripts/collections.ts" />
+var AliveClass = (function () {
+    function AliveClass() {
+    }
+    AliveClass.prototype.onTick = function (time) {
+    };
+    AliveClass.prototype.onBackgroundTick = function (time) {
+        this.onTick(time);
+    };
+    AliveClass.prototype.onStart = function (handler, disabledPermissions) {
+    };
+    AliveClass.prototype.onActionReceived = function (categoryName, jsonedData) {
+    };
+    AliveClass.prototype.onMove = function (oldX, oldY, newX, newY) {
+    };
+    AliveClass.prototype.onRelease = function (currentX, currentY) {
+    };
+    AliveClass.prototype.onPick = function (currentX, currentY) {
+    };
+    AliveClass.prototype.onMenuItemSelected = function (itemName) {
+    };
+    AliveClass.prototype.onConfigureMenuItems = function (menuBuilder) {
+    };
+    AliveClass.prototype.onSpeechRecognitionResults = function (results) { };
+    AliveClass.prototype.onResponseReceived = function (response) {
+    };
+    AliveClass.prototype.onLocationReceived = function (location) {
+    };
+    AliveClass.prototype.onUserActivityStateReceived = function (state) {
+    };
+    AliveClass.prototype.onPlacesReceived = function (places) {
+    };
+    AliveClass.prototype.onHeadphoneStateReceived = function (state) {
+    };
+    AliveClass.prototype.onWeatherReceived = function (weather) {
+    };
+    return AliveClass;
+}());
+//# sourceMappingURL=app.js.map
---
+++ CharacterTemplate/app.ts
@@ -0,0 +1,72 @@
+﻿/// <reference path="Scripts/collections.ts" />
+
+class AliveClass implements IAliveAgent {
+    private resourceManagerHelper: ResourceManagerHelper;
+
+
+    public constructor() {
+
+    }
+
+    onTick(time: number): void {
+
+    }
+
+    onBackgroundTick(time: number) {
+        this.onTick(time);
+    }
+
+    onStart(handler: IManagersHandler, disabledPermissions: string[]): void {
+
+    }
+
+    onActionReceived(categoryName: string, jsonedData: string): void {
+
+    }
+
+    onMove(oldX: number, oldY: number, newX: number, newY: number): void {
+
+    }
+
+    onRelease(currentX: number, currentY: number): void {
+
+    }
+
+    onPick(currentX: number, currentY: number): void {
+
+    }
+
+    onMenuItemSelected(itemName: string): void {
+       
+    }
+
+    onConfigureMenuItems(menuBuilder: IMenuBuilder): void {
+
+    }
+
+    onSpeechRecognitionResults(results: string): void { }
+
+    onResponseReceived(response: string): void {
+
+    }
+
+    onLocationReceived(location: IAliveLocation): void {
+
+    }
+
+    onUserActivityStateReceived(state: IAliveUserActivity) {
+
+    }
+
+    onPlacesReceived(places: IAlivePlaceLikelihood[]): void {
+        
+    }
+
+    onHeadphoneStateReceived(state: number) {
+
+    }
+
+    onWeatherReceived(weather: IAliveWeather) {
+
+    }
+}
